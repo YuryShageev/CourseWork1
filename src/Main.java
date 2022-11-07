7f58c4ae3a1a290ab16e1f04f3d13dfc978d16d4
YuryShageev <schageev.yur@yandex.ru>
2022-11-07 13:36:38 +0300

    Начинаю курсовую

--- src/Main.java
+++ src/Main.java
@@ -3,24 +3,43 @@ public class Main {
     public static void main(String[] args) {
 
         Employees[] employee = new Employees[10];
-        employee[0] = new Employees(new EmployeeName("Винни", "Пух", "Аланович"), 1, 80678);
+        employee[0] = new Employees(new EmployeeName("Пух", "Винни", "Аланович"), 1, 80678);
         employee[1] = new Employees(new EmployeeName("Линдгред", "Карлсон", "Астридович"), 5, 103875);
-        employee[2] = new Employees(new EmployeeName("Леопольд", "Кот", "Аркадьевич"), 9, 67942);
+        employee[2] = new Employees(new EmployeeName("Леопольд", "Кот", "Аркадьевич"), 3, 67942);
+        employee[3] = new Employees(new EmployeeName("Стёпа", "Дядя", "Великанович"), 4, 69832);
+        employee[4] = new Employees(new EmployeeName("Фон", "Ай", "Эпплович"), 2, 137584);
+        employee[5] = new Employees(new EmployeeName("Хонор", "Хаувэй", "Китаевич"), 3, 456352);
+        employee[6] = new Employees(new EmployeeName("Андерсон", "Ганс", "Кристиан"), 2, 45673);
+        employee[7] = new Employees(new EmployeeName("Смит", "Иии", "Вессон"), 1, 99374);
+        employee[8] = new Employees(new EmployeeName("Пистолет", "Пулемёт", "Шпагина"), 5, 70437);
+        employee[9] = new Employees(new EmployeeName("Grand", "Theft", "Auto"), 4, 59673);
 
         for (int i = 0; i < employee.length; i++) {
-            if (employee.length - 1 != null) {
+            System.out.println(employee[i].toString());
 
-                System.out.println(employee[i].toString());
-            } else {
-                System.out.println();
+        }
+
+        int totalSalary = 0;
+        for (int i = 0; i < employee.length; i++) {
+            totalSalary += employee[i].getSalary();
+        }
+        System.out.println(totalSalary);
+
+        int minEmployeeSalary = employee[0].getSalary();
+        for (int i = 0; i < employee.length; i++) {
+            if (minEmployeeSalary > employee[i].getSalary()) {
+                minEmployeeSalary = employee[i].getSalary();
             }
         }
+        System.out.println(minEmployeeSalary);
 
-        int maxSalary = 0;
+        int maxEmployeeSalary = employee[0].getSalary();
         for (int i = 0; i < employee.length; i++) {
-            maxSalary += employee[i].getSalary();
+            if (maxEmployeeSalary < employee[i].getSalary()) {
+                maxEmployeeSalary = employee[i].getSalary();
+            }
         }
-        System.out.println(maxSalary);
+        System.out.println(maxEmployeeSalary);
     }
 
 
